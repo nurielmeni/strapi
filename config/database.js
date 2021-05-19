a027f1a94bf93a21385ed19c9f4e0100106d58d8
Meni Nuriel <nurielmeni@gmail.com>
2021-05-19 21:19:28 +0300

    database

--- config/database.js
+++ config/database.js
@@ -1,18 +1,23 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'hellix_1'),
-        username: env('DATABASE_USERNAME', 'admin'),
-        password: env('DATABASE_PASSWORD', 'Meni1971!'),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-      options: {}
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = () => ({
+    defaultConnection: 'default',
+    connections: {
+        default: {
+            connector: 'bookshelf',
+            settings: {
+                client: 'postgres',
+                host: config.host,
+                port: config.port,
+                database: config.database,
+                username: config.user,
+                password: config.password,
+                ssl: {
+                    rejectUnauthorized: false,
+                },
+            },
+            options: {},
+        },
     },
-  },
 });
