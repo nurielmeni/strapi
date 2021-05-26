fbc782fb674380d73898235f569c4d7a5c07ac2e
Meni Nuriel <nurielmeni@gmail.com>
2021-05-27 02:45:59 +0300

    Spaces Settings

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -14,13 +14,11 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'digitalocean',
       providerOptions: {
-        auth: {
           key: env('SPACES_KEY'),
           secret: env('SPACES_SECRET'),
           region: env('SPACES_REGION'),
           space: env('SPACES_SPACE'),
           cdn: env('SPACES_CDN'),
-        },
       },
     },
   });
