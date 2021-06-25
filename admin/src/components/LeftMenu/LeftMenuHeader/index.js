3202b37a0e119c9b53a8b06fa330e2f10572d813
Meni Nuriel <nurielmeni@gmail.com>
2021-06-25 23:18:25 +0300

    main changes

--- admin/src/components/LeftMenu/LeftMenuHeader/index.js
+++ admin/src/components/LeftMenu/LeftMenuHeader/index.js
@@ -6,7 +6,7 @@ import Wrapper from './Wrapper';
 const LeftMenuHeader = () => (
   <Wrapper>
     <Link to="/" className="leftMenuHeaderLink">
-      <span className="projectName">FLASH Studies</span>
+      <span className="projectName">Theta LMS</span>
     </Link>
   </Wrapper>
 );
