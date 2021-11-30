52177e1b576c7674e32622fbcf0e75c0f1e8717d
Meni Nuriel <nurielmeni@gmail.com>
2021-11-30 23:12:05 +0200

    Instance

--- admin/src/containers/AuthPage/Components/Logo/index.js
+++ admin/src/containers/AuthPage/Components/Logo/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import LogoStrapi from '../../../../assets/images/siteLogo__icon-children.png';
+import LogoStrapi from '../../../../assets/images/strapi_logo.png';
 import Img from './Img';
 
+const LogoImg = process.env.STRAPI_LOGO || LogoStrapi;
+const Organization = process.env.STRAPI_ORGANIZATION || 'Theta LMS';
 const Logo = () => (
   <div>
-    <Img src={LogoStrapi} alt="strapi-logo" />
-    <span>FLASH Studies</span>
+    <Img src={LogoImg} alt="strapi-logo" style={{marginRight: 20}} />
+    <span>{Organization}</span>
   </div>
 );
 
