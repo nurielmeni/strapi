68cb43cd6e9d07bac0d6211da8a443d12f858ce7
Meni Nuriel <nurielmeni@gmail.com>
2021-12-23 00:04:43 +0200

    User Assignment  Validations

--- api/user-sections/services/user-sections.js
+++ api/user-sections/services/user-sections.js
@@ -5,4 +5,35 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+    validateAssignment: async (section, user, id = null) => {
+        if (!section || !user) return true;
+
+        const knex = strapi.connections.default;
+        const result = await knex('user_sections')
+            .where('user', user)
+            .where('section', section)
+            .select('id');
+
+        if (result.length > 0) {
+            return {
+                isValid: false,
+                errMessage: 'Section already assigned to user'
+            }
+        };
+
+        if (id) {
+            const assignment = await knex('user_sections')
+                .where('id', id)
+                .first();
+
+            if (assignment && (assignment.user !== user || assignment.section !== section)) {
+                return {
+                    isValid: false,
+                    errMessage: 'Can not change user or course after assignment'
+                }
+            };
+        }
+        return true;
+    }
+};
