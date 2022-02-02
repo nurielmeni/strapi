839d8aad573dfff81e2a370ca35f6d362e87d7da
Meni Nuriel <nurielmeni@gmail.com>
2022-02-02 02:15:18 +0200

    Group Assignments

--- api/group/controllers/group.js
+++ api/group/controllers/group.js
@@ -49,16 +49,30 @@ module.exports = {
     if (!validMembersId || validMembersId.length < 1) return ctx.badData('At least one valid Member required');
 
 
-    if (courseId) {
-      const course = group.courses.find(c => c.id === +courseId);
-      if (!course) return ctx.badData(`The course id ${courseId}, was not found in group id ${groupId}`);
-      return await strapi.services.group.assignCourses(course.id, validMembersId);
+    if (courseId && Array.isArray(courseId) && courseId.length > 0) {
+      const total = { countAssigned: 0, countSkipped: 0 }
+      for (const currCourseId of courseId) {
+        const course = group.courses.find(c => c.id === currCourseId);
+        if (course) {
+          const res = await strapi.services.group.assignCourses(course.id, validMembersId);
+          total.countAssigned += res.countAssigned;
+          total.countSkipped += res.countSkipped;
+        }
+      }
+      return total;
     }
 
     if (assignmentId) {
-      const assignment = group.assignments.find(a => a.id === +assignmentId);
-      if (!assignment) return ctx.badData(`The assignment id ${assignmentId}, was not found in group id ${groupId}`);
-      return await strapi.services.group.assignAssignments(assignment.id, validMembersId);
+      const total = { countAssigned: 0, countSkipped: 0 }
+      for (const currAssignmentId of courseId) {
+        const assignment = group.assignments.find(a => a.id === currAssignmentId);
+        if (assignment) {
+          const res = await strapi.services.group.assignAssignments(assignment.id, validMembersId);
+          total.countAssigned += res.countAssigned;
+          total.countSkipped += res.countSkipped;
+        }
+      }
+      return total;
     }
   },
 
