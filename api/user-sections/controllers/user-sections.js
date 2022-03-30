3d43e7490ed84aa1ea6ecb8b51489911238c3052
Meni Nuriel <nurielmeni@gmail.com>
2022-03-31 01:44:14 +0300

    Course / Assignment started

--- api/user-sections/controllers/user-sections.js
+++ api/user-sections/controllers/user-sections.js
@@ -32,6 +32,41 @@ module.exports = {
     },
 
     /**
+     * Update a record.
+     *
+     * @return {Object}
+     */
+    async update(ctx) {
+        const { id } = ctx.params;
+
+
+        const { progress: progressBefore } = await strapi.services['user-sections'].findOne({ id }) ?? {};
+        let entity = await strapi.services['user-sections'].update({ id }, ctx.request.body);
+        const { progress: progressAfter } = entity ?? {};
+
+        // Update the start date
+        if (entity && !progressBefore && progressAfter) {
+            entity = await strapi.services['user-sections'].update({ id }, { start_date: new Date() });
+
+            // Add event log to the user "Assignment Start"
+            const eventLogType = await strapi.services['event-log-type'].findOne({ event_type: 'assignment-started' });
+            if (eventLogType?.id) {
+                // section = assignment
+                const { user, section: { id, name } = {} } = entity;
+
+                await strapi.services['event-log'].create({
+                    time: new Date(),
+                    event_log_type: eventLogType.id,
+                    user: user.id,
+                    data: JSON.stringify({ section: { id, name } })
+                });
+            }
+        }
+
+        return sanitizeEntity(entity, { model: strapi.models['user-sections'] });
+    },
+
+    /**
    * Retrieve a records for user.
    *
    * @return {Object}
