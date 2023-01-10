e0ce3e82519b6c6fa27a3a909ef8480f0c063278
Meni Nuriel <nurielmeni@gmail.com>
2023-01-10 22:59:20 +0200

    database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,6 +1,5 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-console.log('database', config);
+// const parse = require('pg-connection-string').parse;
+// const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -8,11 +7,16 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        // host: config.host,
+        // port: config.port,
+        // database: config.database,
+        // username: config.user,
+        // password: config.password,
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
         ssl: {
           rejectUnauthorized: false
         }
