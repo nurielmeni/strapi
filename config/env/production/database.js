973ae65f53aef7fd6eeadff661da477061a560b2
Meni Nuriel <nurielmeni@gmail.com>
2023-01-10 21:37:37 +0200

    log database params

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,6 +1,6 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
-
+console.log('database', config);
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
