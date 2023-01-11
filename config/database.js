3eeb43cbe3f33b27b79e464c16d317a589c3568b
Meni Nuriel <nurielmeni@gmail.com>
2023-01-12 00:38:30 +0200

    email package

--- config/database.js
+++ config/database.js
@@ -7,12 +7,12 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'db'),
+        database: env('DATABASE_NAME', 'system'),
         username: env('DATABASE_USERNAME', 'admin'),
         password: env('DATABASE_PASSWORD', 'Meni1971!'),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: env.bool('DATABASE_SSL', false)
       },
       options: {}
-    },
-  },
+    }
+  }
 });
