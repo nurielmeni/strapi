29c5dcb3430d926a988ae8402a4b78a40e4b6dfb
Meni Nuriel <nurielmeni@gmail.com>
2023-02-27 01:11:59 +0200

    Section Time Accumulator MS

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'production'),
-        username: env('DATABASE_USERNAME', 'production'),
-        password: env('DATABASE_PASSWORD', 'production'),
+        database: env('DATABASE_NAME', 'db'),
+        username: env('DATABASE_USERNAME', 'db'),
+        password: env('DATABASE_PASSWORD', 'db'),
         ssl: env.bool('DATABASE_SSL', false)
       },
       options: {}
