5290e8703df6c7f348cde2e40f998156719d29da
nurielmeni <nurielmeni@gmail.com>
2023-12-21 00:17:59 -0500

    ca cert

--- config/database.js
+++ config/database.js
@@ -58,7 +58,7 @@ module.exports = ({ env }) => {
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
+          ca: env('CA_CERT', undefined),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool(
