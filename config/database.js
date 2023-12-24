b994dfa71e63b0a5c7c7cf334e5b65fd36d10bad
nurielmeni <nurielmeni@gmail.com>
2023-12-24 00:59:00 -0500

    db

--- config/database.js
+++ config/database.js
@@ -20,7 +20,7 @@ module.exports = ({ env }) => {
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool(
             'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
+            false
           ),
         },
       },
