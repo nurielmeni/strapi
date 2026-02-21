8ab468ac3071c3a90dbf2afd954f18b6a4f7d7d9
Nuriel Meni <nurielmeni@gmail.com>
2026-02-22 00:45:38 +0200

    db

--- config/database.js
+++ config/database.js
@@ -1,13 +1,29 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false)
-    }
+module.exports = ({ env }) => {
+  const databaseUrl = env('DATABASE_URL');
+
+  if (databaseUrl) {
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          connectionString: databaseUrl,
+          ssl: false
+        }
+      }
+    };
   }
-});
+
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL', false)
+      }
+    }
+  };
+};
