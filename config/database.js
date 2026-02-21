ee7e84d71f091a9365b2e49e1207712f46d3d326
Nuriel Meni <nurielmeni@gmail.com>
2026-02-22 00:23:30 +0200

    db

--- config/database.js
+++ config/database.js
@@ -1,18 +1,13 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5431),
-        database: env('DATABASE_NAME', 'db'),
-        username: env('DATABASE_USERNAME', 'db'),
-        password: env('DATABASE_PASSWORD', 'db'),
-        ssl: env.bool('DATABASE_SSL', false)
-      },
-      options: {}
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', false)
     }
   }
 });
