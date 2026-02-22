4f839ec58dbd753fbffdfe451116b4f3ddbafa97
Nuriel Meni <nurielmeni@gmail.com>
2026-02-23 00:26:22 +0200

    db

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => {
         client: 'postgres',
         connection: {
           connectionString: databaseUrl,
-          ssl: false
+          ssl: true
         }
       }
     };
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,27 +1,31 @@
-// const parse = require('pg-connection-string').parse;
-// const config = parse(process.env.DATABASE_URL);
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
+module.exports = ({ env }) => {
+  const databaseUrl = env('DATABASE_URL');
+
+  console.log('DATABASE_URL:', databaseUrl); // Log the DATABASE_URL for debugging
+
+  if (databaseUrl) {
+    return {
+      connection: {
         client: 'postgres',
-        // host: config.host,
-        // port: config.port,
-        // database: config.database,
-        // username: config.user,
-        // password: config.password,
+        connection: {
+          connectionString: databaseUrl,
+          ssl: true
+        }
+      }
+    };
+  }
+
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
         host: env('DATABASE_HOST'),
         port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: {
-          rejectUnauthorized: false
-        }
-      },
-      options: {}
+        ssl: env.bool('DATABASE_SSL', false)
+      }
     }
-  }
-});
+  };
+};
