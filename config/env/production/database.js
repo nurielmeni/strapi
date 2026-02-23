be5cae66a529b2968b4f3aa81d4ed60957e90253
Nuriel Meni <nurielmeni@gmail.com>
2026-02-23 02:30:11 +0200

    db

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,26 +1,27 @@
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-  const config = parse(env('DATABASE_URL', ''));
-
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'postgres',
-          host: config.host || env('DATABASE_HOST'),
-          port: config.port || env.int('DATABASE_PORT', 25060),
-          database: config.database || env('DATABASE_NAME'),
-          username: config.user || env('DATABASE_USERNAME'),
-          password: config.password || env('DATABASE_PASSWORD'),
-          ssl: { rejectUnauthorized: false }
-        },
-        options: {
-          ssl: true
+// const parse = require('pg-connection-string').parse;
+// const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        // host: config.host,
+        // port: config.port,
+        // database: config.database,
+        // username: config.user,
+        // password: config.password,
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: {
+          rejectUnauthorized: false
         }
-      }
+      },
+      options: {}
     }
-  };
-};
+  }
+});
